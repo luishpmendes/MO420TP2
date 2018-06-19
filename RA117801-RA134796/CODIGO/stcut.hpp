a5bfb5f094d1089e0d37028289b155d2f38e3939
Luis H. P. Mendes <luishpmendes@gmail.com>
2018-06-19 18:12:18 -0300

    Fixing type of the parameter to the cuts functions.
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

--- RA117801-RA134796/CODIGO/stcut.hpp
+++ RA117801-RA134796/CODIGO/stcut.hpp
@@ -2,8 +2,8 @@
 #include "instance.hpp"
 #include "global.hpp"
 
-void stcuts (const IloBoolVarArray & x, const Instance & inst, std::vector<IloExpr> & STcut, 
-        std::vector<double> & STrhs) {
+void stcuts (const IloBoolVarArray & x,const IloNumArray & var, const Instance & inst, 
+        std::vector<IloExpr> & STcut, std::vector<double> & STrhs) {
     /* TODO */
 }
 
