f5a85a4864714a7bb02f0d4f0d40eb041d2c4ac2
Luis H. P. Mendes <luishpmendes@gmail.com>
2018-06-18 22:14:29 -0300

    Estruturando código para adicionar cuts
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

--- RA117801-RA134796/CODIGO/occut.hpp
+++ RA117801-RA134796/CODIGO/occut.hpp
@@ -1,2 +1,8 @@
 #include "ilcplex/ilocplex.h"
 #include "instance.hpp"
+
+void occuts (const IloBoolVarArray & x, const Instance & inst, std::vector<IloExpr> & OCcut, 
+        std::vector<double> & OCrhs) {
+    /* TODO */
+}
+
