a242a9c7cf3097c822c15fefa421c5a7608fe2f8
Allan Sapucaia <allansapucaia@gmail.com>
2018-06-20 11:37:47 -0300

    Refaz callback de corte, adiciona subtour elimination, e resolve pequenos bugs de solution e params

--- RA117801-RA134796/CODIGO/mf_solver_test.cpp
+++ RA117801-RA134796/CODIGO/mf_solver_test.cpp
@@ -4,11 +4,12 @@
 
 
 void simpleTest() {
-    Graph g(6);
+    FlowGraph g(6);
     g.addFlowEdge(0, 1, 16 );
     g.addFlowEdge(0, 2, 13 );
     g.addFlowEdge(1, 2, 10 );
-    g.addFlowEdge(1, 3, 12.7 );
+    g.addFlowEdge(1, 3, 11 );//testando multi arestas
+    g.addFlowEdge(1, 3, 1.7 );//testando  ponto flutuante
     g.addFlowEdge(2, 1, 4 );
     g.addFlowEdge(2, 4, 14);
     g.addFlowEdge(3, 2, 9 );
@@ -16,8 +17,16 @@ void simpleTest() {
     g.addFlowEdge(4, 3, 7 );
     g.addFlowEdge(4, 5, 4.5);
 
-    double flow = g.DinicMaxflow(0,5);
+    int s = 0;
+    int t = 5;
+    double flow = g.DinicMaxflow(s,t);
     assert(equalDouble(flow , 24.2));
+
+    std::vector<int> truth = {0,1,2,4};
+    std::vector<int> cutSide = g.getCutSide(s);
+
+    for(int i = 0; i < truth.size(); i++) 
+      assert(truth[i] == cutSide[i]);
 }
 int main(int argc, char ** argv) {
   simpleTest();
--- RA117801-RA134796/CODIGO/stcut.hpp
+++ RA117801-RA134796/CODIGO/stcut.hpp
@@ -1,9 +1,28 @@
 #include "ilcplex/ilocplex.h"
 #include "instance.hpp"
 #include "global.hpp"
+#include "mf_solver.hpp"
 
-void stcuts (const IloBoolVarArray & x, const Instance & inst, std::vector<IloExpr> & STcut, 
-        std::vector<double> & STrhs) {
-    /* TODO */
+std::vector<std::vector<int>> getSubTours(const Instance & inst,
+    const std::vector<double> & frac) {
+  std::vector<std::vector<int>> subtours;
+
+  FlowGraph g(inst.size);
+  for(int i = 0; i < inst.edges.size(); i++) {
+    if(frac[i] < EPS)
+      continue;
+    g.addFlowEdge(inst.edges[i].u, inst.edges[i].v, frac[i]);
+    g.addFlowEdge(inst.edges[i].v, inst.edges[i].u, frac[i]);
+  }
+
+  
+  for(int t = 1; t < inst.size; t++) {
+    FlowGraph h = g;
+    double flow = h.DinicMaxflow(0, t);
+    if(flow > 1-EPS)
+      continue;
+    subtours.push_back(h.getCutSide(0));
+  }
+  
+  return subtours;
 }
-
